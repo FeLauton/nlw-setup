e321dd499f695352c6ac1691f6d27c815fe0ed99
Fellipe Lauton <lipelauton@gmail.com>
2023-02-06 22:48:20 -0300

    chore: separado conexão db e rotas, add ao setup dayjs e zod

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,11 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./routes";
 
 const port = 3333;
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors);
-
-app.get("/", async () => {
-  const habits = await prisma.habit.findMany();
-  return habits;
-});
+app.register(appRoutes);
 
 app.listen({ port: port }).then(() => console.log("Server is running"));
